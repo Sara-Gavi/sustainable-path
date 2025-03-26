333a18cefec66f61260eadb6bc676f7b9a0ad182
Sara-Gavi <sara.gavilan.castro@gmail.com>
2025-03-26 20:15:02 +0100

    Creating the App component with base structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,2 +1,24 @@
+import logoApp from "../images/logo_app.png";
+function App() {
+  return (
+    <div>
+      <header className="hero">
+        <section className="hero__content">
+          <img
+            className="content__logo"
+            src={logoApp}
+            alt="Camino Sostenible"
+          />
+          <p className="content__slogan">
+            ¿Quieres conocer el camino de la energía?
+          </p>
+          <div className="content__button">
+            <button className="button__cta">Descubre más</button>
+          </div>
+        </section>
+      </header>
+    </div>
+  );
+}
 
-
+export default App;
